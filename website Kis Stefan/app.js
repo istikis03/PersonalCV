bf1a739560be097663230355dc71cce2f1de0042
istikis03 <32814175+istikis03@users.noreply.github.com>
2020-02-24 12:16:54 +0200

    Add files via upload

---
+++ website Kis Stefan/app.js
@@ -0,0 +1,8 @@
+new fullpage('#fullpage', {
+    autoScrolling: true,
+    navigation: true,
+    navigationTooltips: ['Home', 'About', 'Projects', 'Contact'],
+    anchors: ['section1', 'section2', 'section3', 'section4'],
+    navigationPosition: 'left',
+    showActiveTooltip: true,
+})
